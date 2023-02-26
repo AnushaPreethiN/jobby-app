80436982f890b158e2d5ce237cbadd6fafb00d10
root <anushanagineni123@gmail.com>
2023-02-26 08:41:33 +0000

    jobby app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+import LoginRoute from './components/LoginRoute'
+import HomeRoute from './components/HomeRoute'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import AboutJobItem from './components/AboutJobItem'
+import AllJobs from './components/AllJobs'
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginRoute} />
+    <ProtectedRoute exact path="/" component={HomeRoute} />
+    <ProtectedRoute exact path="/jobs" component={AllJobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={AboutJobItem} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
